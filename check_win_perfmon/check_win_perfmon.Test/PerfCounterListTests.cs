0eaf5505164ead3382cbc58cf93362e4b5c6f64e
Juan Granados <juangranados@gmail.com>
2018-03-15 13:48:08 +0100

    Add unit tests

--- check_win_perfmon/check_win_perfmon.Test/PerfCounterListTests.cs
+++ check_win_perfmon/check_win_perfmon.Test/PerfCounterListTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace check_win_perfmon.Test
@@ -45,6 +46,201 @@ namespace check_win_perfmon.Test
             Assert.AreEqual(perfCounterList.GetGlobalExitCode(), 2);
             perfCounterList.Dispose();
         }
+
+        [TestMethod]
+        public void PerfCounterList_SimulateXMLLoad_StatusOK()
+        {
+            var path = System.IO.Path.GetTempPath() + "\\foo.xml";
+            new XDocument(
+                    new XElement("perfcounters",
+                        new XElement("perfcounter",
+                            new XElement("category", "Processor"),
+                            new XElement("name", "% Processor Time"),
+                            new XElement("instance", "_Total"),
+                            new XElement("friendlyname", "ProcessorTime"),
+                            new XElement("units", "%"),
+                            new XElement("warning", "gt90"),
+                            new XElement("critical", "none"), 
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "PhysicalDisk"),
+                            new XElement("name", "% Idle Time"),
+                            new XElement("instance", "auto"),
+                            new XElement("friendlyname", "IdleTime"),
+                            new XElement("units", "%"),
+                            new XElement("warning", "20"),
+                            new XElement("critical", "15"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "Memory"),
+                            new XElement("name", "Available MBytes"),
+                            new XElement("instance", "none"),
+                            new XElement("friendlyname", "AvailableMBytes"),
+                            new XElement("units", "MB"),
+                            new XElement("warning", "10%"),
+                            new XElement("critical", "5%"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        )
+                    )
+                )
+                .Save(path);
+            var perfCounterList = new PerfCounterList(path);
+            perfCounterList.Calculate(3, 1000);
+            Assert.AreEqual(perfCounterList.GetGlobalExitCode(), 0);
+            perfCounterList.Dispose();
+        }
+
+        [TestMethod]
+        public void PerfCounterList_SimulateXMLLoad_StatusWarning()
+        {
+            var path = System.IO.Path.GetTempPath() + "\\foo.xml";
+            new XDocument(
+                    new XElement("perfcounters",
+                        new XElement("perfcounter",
+                            new XElement("category", "Processor"),
+                            new XElement("name", "% Processor Time"),
+                            new XElement("instance", "_Total"),
+                            new XElement("friendlyname", "ProcessorTime"),
+                            new XElement("units", "%"),
+                            new XElement("warning", "gt1"),//Warning
+                            new XElement("critical", "none"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "PhysicalDisk"),
+                            new XElement("name", "% Idle Time"),
+                            new XElement("instance", "auto"),
+                            new XElement("friendlyname", "IdleTime"),
+                            new XElement("units", "%"),
+                            new XElement("warning", "20"),
+                            new XElement("critical", "15"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "Memory"),
+                            new XElement("name", "Available MBytes"),
+                            new XElement("instance", "none"),
+                            new XElement("friendlyname", "AvailableMBytes"),
+                            new XElement("units", "MB"),
+                            new XElement("warning", "10%"),
+                            new XElement("critical", "5%"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        )
+                    )
+                )
+                .Save(path);
+            var perfCounterList = new PerfCounterList(path);
+            perfCounterList.Calculate(3, 1000);
+            Assert.AreEqual(perfCounterList.GetGlobalExitCode(), 1);
+            perfCounterList.Dispose();
+        }
+
+        [TestMethod]
+        public void PerfCounterList_SimulateXMLLoad_StatusCritical()
+        {
+            var path = System.IO.Path.GetTempPath() + "\\foo.xml";
+            new XDocument(
+                    new XElement("perfcounters",
+                        new XElement("perfcounter",
+                            new XElement("category", "Processor"),
+                            new XElement("name", "% Processor Time"),
+                            new XElement("instance", "_Total"),
+                            new XElement("friendlyname", "ProcessorTime"),
+                            new XElement("units", "%"),
+                            new XElement("warning", "gt1"),//Warning
+                            new XElement("critical", "none"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "PhysicalDisk"),
+                            new XElement("name", "% Idle Time"),
+                            new XElement("instance", "auto"),
+                            new XElement("friendlyname", "IdleTime"),
+                            new XElement("units", "%"),
+                            new XElement("warning", "101"),
+                            new XElement("critical", "100"),//Critical
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "Memory"),
+                            new XElement("name", "Available MBytes"),
+                            new XElement("instance", "none"),
+                            new XElement("friendlyname", "AvailableMBytes"),
+                            new XElement("units", "MB"),
+                            new XElement("warning", "10%"),
+                            new XElement("critical", "5%"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        )
+                    )
+                )
+                .Save(path);
+            var perfCounterList = new PerfCounterList(path);
+            perfCounterList.Calculate(3, 1000);
+            Assert.AreEqual(perfCounterList.GetGlobalExitCode(), 2);
+            perfCounterList.Dispose();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PerfCounterList_SimulateBadXMLLoad_Exception()
+        {
+            var path = System.IO.Path.GetTempPath() + "\\foo.xml";
+            new XDocument(
+                    new XElement("perfcounters",
+                        new XElement("perfcounter",
+                            new XElement("category", "Processor"),
+                            new XElement("name", "% Processor Time"),
+                            new XElement("instance", "_Total"),
+                            new XElement("friendlyname", "ProcessorTime"),
+                            new XElement("units", "%"),
+                            //new XElement("warning", "gt1"),
+                            new XElement("critical", "none"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "PhysicalDisk"),
+                            new XElement("name", "% Idle Time"),
+                            new XElement("instance", "auto"),
+                            new XElement("friendlyname", "IdleTime"),
+                            new XElement("units", "%"),
+                            new XElement("warning", "101"),
+                            new XElement("critical", "100"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        ),
+                        new XElement("perfcounter",
+                            new XElement("category", "Memory"),
+                            new XElement("name", "Available MBytes"),
+                            new XElement("instance", "none"),
+                            new XElement("friendlyname", "AvailableMBytes"),
+                            new XElement("units", "MB"),
+                            new XElement("warning", "10%"),
+                            new XElement("critical", "5%"),
+                            new XElement("min", "0"),
+                            new XElement("max", "100")
+                        )
+                    )
+                )
+                .Save(path);
+            using (var perfCounterList = new PerfCounterList(path))
+            {
+                perfCounterList.Calculate(3, 1000);
+                perfCounterList.Dispose();
+            }
+        }
+
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
